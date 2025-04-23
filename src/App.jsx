7461c9d6ede30637ce3ccb46c982b85d7f9cc52a
hevardhan <paatukkorupulavan@gmail.com>
2025-04-23 19:37:13 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Home from './pages/Home'
+import Homedup from './pages/Homedup'
+
+function App() {
+
+  return (
+    <>
+      <Home />
+      {/* <Homedup /> */}
+    </>
+  )
+}
+
+export default App
